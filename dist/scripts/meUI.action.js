832513247434dd016d3d1c916f01a01578253070
wzs28150 <wz666s@163.com>
2016-12-29 15:05:23 +0800

    updata

--- dist/scripts/meUI.action.js
+++ dist/scripts/meUI.action.js
@@ -127,8 +127,19 @@ meui.Nav = (function() {
     setJqueryMap = function() {};
     navToggle = function() {
         $('header .navbar-toggle').click(function() {
-            $('header nav').slideToggle();
+            if ($("header nav").is(":hidden")) {
+                $("header nav").slideDown(); //如果元素为隐藏,则将它显现
+            } else {
+                $("header nav").slideUp(); //如果元素为显现,则将其隐藏
+            }
             $("header nav a").removeClass("in-viewport");
+            $("header nav a").click(function() {
+                if ($("header nav").is(":hidden")) {
+                    $("header nav").slideDown(); //如果元素为隐藏,则将它显现
+                } else {
+                    $("header nav").slideUp(); //如果元素为显现,则将其隐藏
+                }
+            });
             $("header nav a").each(function(e) {
                 var a = $(this);
                 a.hasClass("in-viewport") || setTimeout(function() {

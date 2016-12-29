15e8d38a032c6a267b39abefacedaac0761001f3
wzs28150 <wz666s@163.com>
2016-12-29 12:43:22 +0800

    updata

--- dist/scripts/meUI.action.js
+++ dist/scripts/meUI.action.js
@@ -63,39 +63,10 @@ meui.Index_action = (function() {
 
         }
     };
-    initModule = function($data) {
-        meui.Nav.initModule(0);
+    initModule = function($data, $setting) {
+        //meui.Nav.initModule(0);
         indexBanner();
         propicColor();
-    };
-    return {
-        initModule: initModule,
-    };
-}());
-meui.Service_action = (function() {
-    "use strict";
-    var stateMap = {},
-        jqueryMap = {},
-        setJqueryMap,
-        initModule;
-    setJqueryMap = function() {};
-    initModule = function($data, $setting) {
-        meui.Nav.initModule(2);
-    };
-    return {
-        initModule: initModule,
-    };
-}());
-meui.Cooper_action = (function() {
-    "use strict";
-    var stateMap = {},
-        jqueryMap = {},
-        setJqueryMap,
-        initModule;
-    setJqueryMap = function() {};
-    initModule = function($data, $setting) {
-        meui.Nav.initModule(3);
-        meui.loadMore.initModule(4, 2, '.cooper-list .item', '.cooper-list .loadmore');
     };
     return {
         initModule: initModule,
@@ -115,56 +86,7 @@ meui.About_action = (function() {
         initModule: initModule,
     };
 }());
-meui.Contact_action = (function() {
-    "use strict";
-    var stateMap = {},
-        jqueryMap = {},
-        setJqueryMap,
-        initModule, createMap, setMapEvent, addMapControl;
-    setJqueryMap = function() {};
-    createMap = function() {
-            meui.Nav.initModule(5);
-            var map = new BMap.Map("dituContent"); //在百度地图容器中创建一个地图
-            var point = new BMap.Point(126.638206, 45.855999); //定义一个中心点坐标
-            map.centerAndZoom(point, 17); //设定地图的中心点和坐标并将地图显示在地图容器中
-            window.map = map; //将map变量存储在全局
-        }
-        //地图事件设置函数：
-    setMapEvent = function() {
-            map.enableDragging(); //启用地图拖拽事件，默认启用(可不写)
-            map.enableScrollWheelZoom(); //启用地图滚轮放大缩小
-            map.enableDoubleClickZoom(); //启用鼠标双击放大，默认启用(可不写)
-            map.enableKeyboard(); //启用键盘上下左右键移动地图
-        }
-        //地图控件添加函数：
-    addMapControl = function() {
-        //向地图中添加缩放控件
-        var ctrl_nav = new BMap.NavigationControl({
-            anchor: BMAP_ANCHOR_TOP_LEFT,
-            type: BMAP_NAVIGATION_CONTROL_LARGE
-        });
-        map.addControl(ctrl_nav);
-        //向地图中添加缩略图控件
-        var ctrl_ove = new BMap.OverviewMapControl({
-            anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
-            isOpen: 1
-        });
-        map.addControl(ctrl_ove);
-        //向地图中添加比例尺控件
-        var ctrl_sca = new BMap.ScaleControl({
-            anchor: BMAP_ANCHOR_BOTTOM_LEFT
-        });
-        map.addControl(ctrl_sca);
-    }
-    initModule = function($data, $setting) {
-        createMap(); //创建地图
-        setMapEvent(); //设置地图事件
-        addMapControl(); //向地图添加控件
-    };
-    return {
-        initModule: initModule,
-    };
-}());
+
 meui.Backtotop = (function() {
     "use strict";
     var stateMap = {},
@@ -203,8 +125,20 @@ meui.Nav = (function() {
     var stateMap = {},
         jqueryMap = {},
         setJqueryMap,
-        initModule, navOn;
+        initModule, navOn, navToggle;
     setJqueryMap = function() {};
+    navToggle = function() {
+        $('header .navbar-toggle').click(function() {
+            $('header nav').slideToggle();
+            $("header nav a").removeClass("in-viewport");
+            $("header nav a").each(function(e) {
+                var a = $(this);
+                a.hasClass("in-viewport") || setTimeout(function() {
+                    a.addClass("in-viewport")
+                }, 100 * e)
+            });
+        });
+    };
     initModule = function(i) {
         if (i != undefined) {
             $('header .inner nav a').removeClass('on');
@@ -216,6 +150,14 @@ meui.Nav = (function() {
                 $(this).addClass('on');
             });
         }
+        if ($(window).width() < 960) {
+            navToggle();
+        }
+        $(window).resize(function() {
+            if ($(window).width() < 960) {
+                navToggle();
+            }
+        });
     };
     return {
         initModule: initModule,
@@ -236,68 +178,3 @@ meui.Callback = (function() {
         initModule: initModule,
     };
 }());
-(function($) {
-    "use strict";
-    $.fn.support = function(opt) {
-        var opts = $.extend({
-            spanclass: ".span.c",
-            changeTime: 1000,
-            onList: Array(2, 5, 9, 12),
-        }, opt || {});
-        var target = this;
-        //opts.height
-        //初始化函数
-        var span = target.find(opts.spanclass);
-        var text = target.find('.text');
-        var targetH = $(target).offset().top;
-        var changec, changet;
-        //console.log(targetH);
-        $(window).scroll(function() {
-            if ($(target).hasClass('in-viewport')) {
-                initial();
-            } else {
-                chushihua();
-            }
-        });
-
-        function initial() {
-            //alert(opts.onList.length)
-            changeColor();
-            textShow();
-        }
-
-        function chushihua() {
-            window.clearTimeout(changec);
-            window.clearTimeout(changet);
-            clearColor();
-            cleartextShow();
-        }
-        //2 5 9 12
-        function changeColor() {
-            span.each(function(i) {
-                changec = setTimeout(function() {
-                    span.eq(i).addClass('on');
-                }, opts.changeTime * i);
-            })
-        }
-
-        function clearColor() {
-            span.removeClass('on');
-        }
-
-        function textShow() {
-            text.each(function(i) {
-                //alert(i)
-                changet = setTimeout(function() {
-                    text.eq(i).fadeIn();
-                }, opts.changeTime * i);
-            });
-        }
-
-        function cleartextShow() {
-            //console.log('done');
-            text.hide();
-        }
-        return this;
-    };
-})(jQuery);

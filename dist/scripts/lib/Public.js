c9e266ccf896ed8c6b1c8d758f857d48c5cbfd55
wzs28150 <wz666s@163.com>
2017-01-23 15:03:23 +0800

    updata

--- dist/scripts/lib/Public.js
+++ dist/scripts/lib/Public.js
@@ -15,7 +15,7 @@ meui.Public_action = (function() {
 
             }
           });
-          
+
         }
     });
 
@@ -63,31 +63,35 @@ meui.Nav = (function() {
 		setJqueryMap,
 		initModule,
 		navOn,
-		navToggle;
+		navToggle,navaTogge;
 	setJqueryMap = function() {};
 	navToggle = function() {
-		$('header .navbar-toggle').click(function() {
+		$('header .navbar-toggle').click(function(e) {
+      e.stopPropagation();
 			if ($("header nav").is(":hidden")) {
 				$("header nav").slideDown(); //如果元素为隐藏,则将它显现
 			} else {
 				$("header nav").slideUp(); //如果元素为显现,则将其隐藏
 			}
-			$("header nav a").removeClass("in-viewport");
-			$("header nav a").click(function() {
-				if ($("header nav").is(":hidden")) {
-					$("header nav").slideDown(); //如果元素为隐藏,则将它显现
-				} else {
-					$("header nav").slideUp(); //如果元素为显现,则将其隐藏
-				}
-			});
-			$("header nav a").each(function(e) {
-				var a = $(this);
-				a.hasClass("in-viewport") || setTimeout(function() {
-					a.addClass("in-viewport")
-				}, 100 * e)
-			});
+
 		});
 	};
+  navaTogge = function(){
+    $("header nav a").removeClass("in-viewport");
+    $("header nav a").click(function() {
+      if ($("header nav").is(":hidden")) {
+        $("header nav").slideDown(); //如果元素为隐藏,则将它显现
+      } else {
+        $("header nav").slideUp(); //如果元素为显现,则将其隐藏
+      }
+    });
+    $("header nav a").each(function(e) {
+      var a = $(this);
+      a.hasClass("in-viewport") || setTimeout(function() {
+        a.addClass("in-viewport")
+      }, 100 * e)
+    });
+  }
 	initModule = function(i) {
 		if (i != undefined) {
 			$('header nav a').removeClass('on');
@@ -101,10 +105,12 @@ meui.Nav = (function() {
 		}
 		if ($(window).width() < 960) {
 			navToggle();
+      navaTogge();
 		}
 		$(window).resize(function() {
 			if ($(window).width() < 960) {
 				navToggle();
+        navaTogge();
 			}
 		});
 	};
